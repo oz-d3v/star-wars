c25b6007cdd423e7f597e26918ae2f3bb05a4aff
Osama Zaman <osamazaman@outlook.com>
2022-10-30 18:43:03 +0500

    starwars app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import moment from "moment/moment";
 
-function App() {
+export const App = () => {
+  const [apiData, setApiData] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const [description, setDesciption] = useState({
+    title: null,
+    description: null,
+    releaseDate: null,
+  });
+
+  useEffect(() => {
+    fetch("https://swapi.py4e.com/api/films")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setIsLoading(false);
+        setApiData(data.results);
+      });
+  }, []);
+
+  const handleClick = (index) => {
+    // console.log("checkkksafd", index.target.outerText);
+    const title = index.target.outerText;
+    let descriptiveIndex;
+    for (let i = 0; i < apiData.length; i++) {
+      if (title === apiData[i].title) {
+        descriptiveIndex = i;
+      }
+    }
+    setDesciption({
+      title: apiData[descriptiveIndex].title,
+      description: apiData[descriptiveIndex].opening_crawl,
+      releaseDate: apiData[descriptiveIndex].release_date,
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="star_wars_app">
+      {isLoading ? (
+        <div className="loading">
+          <p> loading</p>
+        </div>
+      ) : (
+        <>
+          <div className="movies_title_container">
+            {apiData.map((data, index) => {
+              return (
+                <>
+                  <div key={index}>
+                    <p
+                      className="movies_item"
+                      onClick={(args) => handleClick(args)}
+                    >
+                      {data.title}
+                    </p>
+                  </div>
+                </>
+              );
+            })}
+          </div>
+
+          <div
+            className="details_container"
+            style={
+              description != null ? { display: "block" } : { display: "none" }
+            }
+          >
+            <div className="title">{description.title}</div>
+            <div className="description">{description.description}</div>
+            <div
+              className="releaseDate"
+              style={
+                description.releaseDate == null
+                  ? { display: "none" }
+                  : { display: "block" }
+              }
+            >
+              {moment(description.releaseDate).format("MM/DD/YYYY")}
+            </div>
+            {/* wrap this with moment mm/dd/yyyy*/}
+          </div>
+        </>
+      )}
     </div>
   );
-}
+};
 
 export default App;
+
+//
